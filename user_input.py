5b3882e9d22202e872a5dabe3e19bd7bff2763f0
EliudMash <eliud6747@gmail.com>
2024-03-13 12:15:23 +0300

    User Input assignment

---
+++ user_input.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+# This is a code that requests for user details and prints them to screen
+
+# Ask for name
+name = input("What is your name? ")
+
+# Ask for age
+age = input("What is your age? ")
+
+# Ask for location
+location = input("What is your location? ")
+
+# Print to screen
+print("Hello {}, you are {} years old and live in {}".format(name, age, location))
